4056283d4ebd59c4e08d5b0cfff39ef202d9cf7a
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2024-07-15 08:19:39 +0200

    tried to build apk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://akahdlwzvzzqsegsjykh.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrYWhkbHd6dnp6cXNlZ3NqeWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAzNTMwNTYsImV4cCI6MjAzNTkyOTA1Nn0.a0w2dujcEoKWvo9Bam_xhV_qHnUKZSsIHUYvFA8Ldzo',
 };
